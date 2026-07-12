108007da6aa1e53a0926ee64f8c4081c8c9607dc
Gaetan <gaetan@hserver.local>
2026-07-12 16:35:06 +0200

    Improve todo_done form

--- todo_done.sql
+++ todo_done.sql
@@ -3,32 +3,57 @@ select 'title' as component, 'Todo closer' as contents;
 
 select 'form' as component, 
 	'closer' as id,
+	'Mark as Done' as validate,
 	'todo_done_act.sql' as action
 ;
 
 
-select 'id' as name, 'hidden' as type, $todo_id as value
+select 'text' as type,
+	'id' as name,
+	'hidden' as type,
+	$todo_id as value
 ;
 
-select 'tag' as name, 'Tag' as label, 3 as width,
+select 'text as type,
+	'tag' as name,
+	'Tag' as label,
+	3 as width,
+	TRUE AS readonly,
 	(select asset from Log_M WHERE ID=$todo_id) as value
 ;
 
-select 'action' as name, 'Action' as label, 3 as width,
+select 'text' as type,
+	action' as name,
+	'Action' as label,
+	3 as width,
+	TRUE AS readonly,
 	(select action from Log_M WHERE ID=$todo_id) as value
 ;
 
-select 'Date' as name, 'date' as type, date('now') as value, 3 as width
+select 'date' as type,
+	'Date' as name,
+	date('now') as value,
+	3 as width
 ;
 
-select 'user' as name, 'select' as type, "Executor" as label, 4 as width,
+select 'text' as type,
+	'user' as name,
+	'select' as type,
+	"Executor" as label,
+	4 as width,
 	(SELECT json_group_array( json_object( 'value', ID, 'label', Name ) )  FROM People) as options
 ;
 
-select 'comment' as name, 'Comment' as label, 3 as rows, 'textarea' as type;
+select 'textarea' as type,
+	'comment' as name,
+	'Comment' as label,
+	3 as rows
+;
 
-SELECT 'button' as component, 'closer' as form;
+SELECT 'button' as component,
+	'closer' as form;
 SELECT 
     'Annuler'        AS title,
     'sub_todos.sql'      AS link,     -- URL de redirection au clic
-    'secondary'      AS color;    -- Couleur grise standard pour une annulation
+    'secondary'      AS color    -- Couleur grise standard pour une annulation
+;
